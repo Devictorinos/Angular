710f563da63a702d715e8a20ef54fa4140e76e72
V1ktorino <viktorlubchuk@gmail.com>
2014-12-15 21:40:00 +0200

    commit

--- BookLessones/http_service/server.php
+++ BookLessones/http_service/server.php
@@ -3,8 +3,18 @@
 
 
 
-if ($_GET) {
- 
+if ($_GET || $_POST) {
+ /*   $data  = json_encode($_POST);
+    echo "<pre>" . print_r($data, true) . "</pre>";*/
+/*    $params = json_decode(file_get_contents('php://input'));
+    var_dump($params);*/
+
+    if ($_POST) {
+
+        $_POST['id'] = time();
+        
+    }
+
     $notes = [
       0=> array( 'id'=> 1, 'label'=> 'First Note', 'author'=> 'Shyam'),
       1=> array( 'id'=> 2, 'label'=> 'Second Note', 'author'=> 'Brad'),
@@ -12,6 +22,11 @@ if ($_GET) {
       3=> array( 'id'=> 4, 'label'=> 'Last Note', 'author'=> 'Shyam'),
       4=> array( 'id'=> 5, 'label'=> 'Really the last Note', 'author'=> 'Shyam')
     ];
+
+    if ($_POST) {
+        $notes[] = $_POST;
+    }
+ 
 
     echo json_encode($notes);
 }
